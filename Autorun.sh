a172ecb254580e0ddea66a0e3b26864d5e8e9950
jonyrock <ilms@live.ru>
2014-03-07 13:14:39 +0400

    main Autorun script and formating

---
+++ Autorun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 5
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/HomeEnd.sh
+. $DIR/Compiz.sh
--- HomeEnd.sh
+++ HomeEnd.sh
@@ -3,8 +3,7 @@
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 
 echo -ne "Set Home/End keys ... "
-sleep 1
+#sleep 2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-xmodmap $DIR/Xmodmap
+xmodmap "/home/alex/LinuxThinkpadScripts/Xmodmap"
 echo "OK"
-
