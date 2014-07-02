dbf2585c4d552a8baa6b09ee150ebde41e629a71
jonyrock <ilms@live.ru>
2014-07-02 14:03:46 +0300

    note about workspaces

--- HomeEnd.sh
+++ HomeEnd.sh
@@ -3,7 +3,7 @@
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 
 echo -ne "Set Home/End keys ... "
-#sleep 2
+sleep 2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 xmodmap "/home/alex/LinuxThinkpadScripts/Xmodmap"
 echo "OK"
