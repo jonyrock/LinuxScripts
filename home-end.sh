6bed8010e26f096a8df7243d58e9ad48b4b97546
Alexey Velikiy <av@corpglory.com>
2020-08-03 18:21:51 +0200

    style fixes + home-end

--- home-end.sh
+++ home-end.sh
@@ -6,6 +6,6 @@ echo -ne "Set Home/End keys in while ... "
 while(true)
 do
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	xmodmap "/home/alex/LinuxThinkpadScripts/Xmodmap"
+	xmodmap "Xmodmap"
 	sleep 2
 done
---
+++ touchpad-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+synclient TapButton1=0
+synclient TapButton2=0
+synclient TapButton3=0
