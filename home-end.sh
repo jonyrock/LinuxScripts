c4862454ba01609ba39b15cebf295de69530d18c
Alexey Velikiy <av@corpglory.com>
2020-08-07 02:35:37 +0200

    simple home-end

--- home-end.sh
+++ home-end.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-
-echo -ne "Set Home/End keys in while ... "
-while(true)
-do
-	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	xmodmap "Xmodmap"
-	sleep 2
-done
+xmodmap "Xmodmap"
